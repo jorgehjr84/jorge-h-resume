ad8bd5e655d5a638a8e10645887c7bec684fec1c
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2017-06-11 11:48:15 -0400

    Request line animating from center out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,10 @@ import { SkillsComponent } from './components/skills/skills.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None
 
 })
 export class AppComponent {
   title = "Jorge Hernandez's Sequence Diagram Resume";
+  
 }
