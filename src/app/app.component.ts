8029be1935f4a4aab3d3770de2b57a1c6b0b53ee
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2017-06-11 21:25:42 -0400

    Animations working for all 3 experiences

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, trigger, animate, state, style, keyframes, transition } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 
 import { HeaderComponent } from './components/header/header.component';
@@ -13,10 +13,114 @@ import { SkillsComponent } from './components/skills/skills.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+      trigger('activateExperience', [
+       state('inactive', style({
+        opacity: .25,
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        opacity: 1,
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('200ms ease-in')),
+      transition('active => inactive', animate('200ms ease-out  '))
+    ]),
+    trigger('requestSkills', [
+      state('inactive', style({
+        transform: 'scale(0)'
+      })),
+      state('active', style({
+       transform: 'scale(.378)'
+
+      })),
+      transition('inactive => active', animate('200ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out  '))
+    ]),
+    trigger('displayReturnedSkills', [
+      state('inactive', style({
+        transform: 'translateY(-600%)'
+      })),
+      state('active', style({
+        transform: 'translateY(0)'
+      })),
+      transition('inactive => active', animate('900ms ease-in')),
+      transition('active => inactive', animate('900ms ease-out'))
+    ])
+  ]
 
 })
 export class AppComponent {
   title = "Jorge Hernandez's Sequence Diagram Resume";
   
+    state1: string = 'inactive';
+    state2: string = 'inactive';
+    state3: string = 'inactive';
+
+  toggleReturn(event) {
+    console.log(event)
+    if(event == 1) {
+      this.state1 = (this.state1 === 'inactive' ? 'active' : 'inactive');
+      this.state2 = 'inactive';
+      this.state3 = 'inactive';
+    }else if (event == 2) {
+      this.state2 = (this.state2 === 'inactive' ? 'active' : 'inactive');
+      this.state1 = 'inactive';
+      this.state3 = 'inactive';
+    } else {
+      this.state3 = (this.state3 === 'inactive' ? 'active' : 'inactive');
+      this.state1 = 'inactive';
+      this.state2 = 'inactive';
+    }  
+  }
 }
+
+
+// @Component({
+//   selector: 'medullan-experience',
+//   templateUrl: './medullanExp.component.html',
+//   styleUrls: ['./medullanExp.component.css'],
+//   animations: [
+//     trigger('requestSkills', [
+//       state('inactive', style({
+//         transform: 'scale(0)'
+//       })),
+//       state('active', style({
+//         transform: 'scale(1)'
+//       })),
+//       transition('inactive => active', animate('200ms ease-in')),
+//       transition('active => inactive', animate('100ms ease-out  '))
+//     ]),
+//     trigger('displayReturnedSkills', [
+//       state('inactive', style({
+//         transform: 'translateY(900%)'
+//       })),
+//       state('active', style({
+//         transform: 'translateY(0)'
+//       })),
+//       transition('inactive => active', animate('900ms ease-in')),
+//       transition('active => inactive', animate('900ms ease-out'))
+//     ]),
+//     trigger('activateExperience', [
+//       state('inactive', style({
+//         opacity: .25,
+//         transform: 'scale(1)'
+//       })),
+//       state('active', style({
+//         opacity: 1,
+//         transform: 'scale(1.1)'
+//       })),
+//       transition('inactive => active', animate('200ms ease-in')),
+//       transition('active => inactive', animate('200ms ease-out  '))
+//     ])
+//   ]
+// })
+// export class MedullanExpComponent {
+//   state: string = 'inactive';
+
+//   toggleReturn() {
+//     this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+//   }
+
+// }
--- src/app/components/medullanExp/medullanExp.component.ts
+++ src/app/components/medullanExp/medullanExp.component.ts
@@ -3,41 +3,7 @@ import { Component, trigger, animate, state, style, keyframes, transition } from
 @Component({
   selector: 'medullan-experience',
   templateUrl: './medullanExp.component.html',
-  styleUrls: ['./medullanExp.component.css'],
-  animations: [
-    trigger('requestSkills', [
-      state('inactive', style({
-        transform: 'scale(0)'
-      })),
-      state('active', style({
-        transform: 'scale(1)'
-      })),
-      transition('inactive => active', animate('200ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out  '))
-    ]),
-    trigger('displayReturnedSkills', [
-      state('inactive', style({
-        transform: 'translateY(900%)'
-      })),
-      state('active', style({
-        transform: 'translateY(0)'
-      })),
-      transition('inactive => active', animate('900ms ease-in')),
-      transition('active => inactive', animate('900ms ease-out'))
-    ]),
-    trigger('activateExperience', [
-      state('inactive', style({
-        opacity: .25,
-        transform: 'scale(1)'
-      })),
-      state('active', style({
-        opacity: 1,
-        transform: 'scale(1.1)'
-      })),
-      transition('inactive => active', animate('200ms ease-in')),
-      transition('active => inactive', animate('200ms ease-out  '))
-    ])
-  ]
+  styleUrls: ['./medullanExp.component.css']
 })
 export class MedullanExpComponent {
   state: string = 'inactive';
